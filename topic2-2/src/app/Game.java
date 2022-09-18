af51893cb98d824f50b6561b1765ca6c1489464a
Eric-Johnston <eric.mjohnston@protonmail.com>
2022-09-18 16:02:08 -0400

    2-2 complete

---
+++ topic2-2/src/app/Game.java
@@ -0,0 +1,21 @@
+package app;
+
+/**
+ * Implement game via main()
+ * @author Eric Johnston
+ * @version 1.0
+ */
+public class Game
+{
+	public static void main(String[] args)
+	{
+		Bomb weapon1 = new Bomb();
+		Gun weapon2 = new Gun();
+		weapon1.activate(true);
+		weapon2.activate(true);
+		weapon1.fireWeapon(10);
+		weapon2.fireWeapon(5);
+		weapon1.fireWeapn();
+		weapon2.fireWeapon();
+	}
+}
